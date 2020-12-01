727f157d7f190838ff0c9eaafae5cbdd2f00d548
Martin <asdf@example.com>
2020-12-01 09:06:44 -0500

    updated script to use lists of objects in order to begin transition to variable number of buttons opposed to a static number

--- Text_Adventure.py
+++ Text_Adventure.py
@@ -7,17 +7,12 @@ class Text_Dungeon(tk.Frame):
         self.maxcolval = 1
         self.mincolval = 0
         
-        self.minput = 0
+        self.textvars = []
+        self.minput = 0                
         
-        self.atext = tk.StringVar()
-        self.btext = tk.StringVar()
-        self.ctext = tk.StringVar()
-        self.dtext = tk.StringVar()
-        
-        self.atext.set('nobadword')
-        self.btext.set('nobadword')
-        self.ctext.set('nobadword')
-        self.dtext.set('nobadword')
+        for i in range(0, 4):
+            self.textvars.append(tk.StringVar())
+            self.textvars[i].set('nobadword')                
         
         super().__init__(master)
         self.master = master
@@ -43,10 +38,10 @@ class Text_Dungeon(tk.Frame):
         #set the second column to expand while the first stays put
         self.columnconfigure(self.maxcolval, weight=1)
 
-    def create_widgets(self):
-        self.create_user_selButo()
-        self.create_user_selText()
+    def create_widgets(self):        
         self.create_mainPrompt()
+        self.create_user_selButo()
+        self.create_user_selText()                
         
     def create_mainPrompt(self):
         self.mPrompt = tk.Listbox(self, bg='black', fg='white', highlightcolor='red', highlightbackground='red',
@@ -56,57 +51,44 @@ class Text_Dungeon(tk.Frame):
         self.mPrompt.configure(yscrollcommand=sb)
         
     def create_user_selButo(self):
-        self.butoA = tk.Button(self, text="A", bg='black', fg="red",
-                              command=self.acmd)
-        self.butoA.grid(row=(self.maxrowval-3), column=self.mincolval, sticky='ewns')
-        self.butoB = tk.Button(self, text="B", bg='black', fg="red",
-                              command=self.bcmd)
-        self.butoB.grid(row=(self.maxrowval-2), column=self.mincolval, sticky='ewns')
-        self.butoC = tk.Button(self, text="C", bg='black', fg="red",
-                              command=self.ccmd)
-        self.butoC.grid(row=(self.maxrowval-1), column=self.mincolval, sticky='ewns')
-        self.butoD = tk.Button(self, text="D", bg='black', fg="red",
-                              command=self.dcmd)
-        self.butoD.grid(row=self.maxrowval, column=self.mincolval, sticky='ewns')
+        
+        self.buttons = []
+        self.cmdHandlers = []
+        
+        for i in range(0,4):
+            self.cmdHandlers.append(ButtonCmd(i, self.textvars[i], self.mPrompt))
+            self.buttons.append(tk.Button(self, text=str(i+1), bg='black', fg="red", command=self.cmdHandlers[i].button_cmd))
+            self.buttons[i].grid(row=i+1, column=self.mincolval, sticky='ewns')
+            
         
     def create_user_selText(self):
-        self.textA = tk.Label(self, textvariable=self.atext, bg='black', fg='white', anchor='nw')
-        self.textA.grid(row=(self.maxrowval-3), column=self.maxcolval, sticky='ewns')
-        self.textB = tk.Label(self, textvariable=self.btext, bg='black', fg='white', anchor='nw')
-        self.textB.grid(row=(self.maxrowval-2), column=self.maxcolval, sticky='ewns')
-        self.textC = tk.Label(self, textvariable=self.ctext, bg='black', fg='white', anchor='nw')
-        self.textC.grid(row=(self.maxrowval-1), column=self.maxcolval, sticky='ewns')
-        self.textD = tk.Label(self, textvariable=self.dtext, bg='black', fg='white', anchor='nw')
-        self.textD.grid(row=(self.maxrowval), column=self.maxcolval, sticky='ewns')
-
-    def acmd(self):
-        self.mPrompt.insert(self.minput, 'nafam' + '\n')
-        self.minput += 1
-        if 'FUCK' in self.atext.get():
-            self.atext.set("nobadword")
-        else:
-            self.atext.set("FUCK A")
         
-    def bcmd(self):
-        if 'FUCK' in self.btext.get():
-            self.btext.set("nobadword")
-        else:
-            self.btext.set("FUCK B")
+        self.text_labels = []
         
-    def ccmd(self):
-        if 'FUCK' in self.ctext.get():
-            self.ctext.set("nobadword")
-        else:
-            self.ctext.set("FUCK C")
-        
-    def dcmd(self):
-        if 'FUCK' in self.dtext.get():
-            self.dtext.set("nobadword")
-        else:
-            self.dtext.set("FUCK D")
+        for i in range(0,4):
+            self.text_labels.append(tk.Label(self, textvariable=self.textvars[i], bg='black', fg='white', anchor='nw'))
+            self.text_labels[i].grid(row=i+1, column=self.maxcolval, sticky='ewns')
+              
 
 class Choices_Var(tk.Scrollbar):
     pass
+    
+class ButtonCmd():
+    
+    def __init__(self, ID, Text_Var, Main_Prompt):
+        self.ID = ID
+        self.Text_Var = Text_Var
+        self.Main_Prompt = Main_Prompt
+    
+    def button_cmd(self):
+    
+        innum = self.Main_Prompt.size()
+        self.Main_Prompt.insert(innum + 1, 'nafam' + '\n')        
+        if 'FUCK' in self.Text_Var.get():
+            self.Text_Var.set("nobadword")
+        else:
+            self.Text_Var.set("FUCK " + str(self.ID))
+ 
 
 
 def runprog():
