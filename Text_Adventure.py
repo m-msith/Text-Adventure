865d4841e31e2e6d4df8c5d2bf4ff2610082a076
Martin <martin@desktop.fake>
2020-12-07 07:40:42 -0500

    additions to add a scrolling bar to the bottom quadrent in order to attempt to maintain proportions through screen resizes as well as choice options

--- Text_Adventure.py
+++ Text_Adventure.py
@@ -42,54 +42,88 @@ class Text_Dungeon():
         #set to the only interior spot
         self.panel_one.mPrompt.grid(row=0, column=0, sticky='nsew')
         
+        #add in a scrollbar 
         sb = tk.Scrollbar(self.panel_one.mPrompt)
-        self.panel_one.mPrompt.configure(yscrollcommand=sb)
+        self.panel_one.mPrompt.configure(yscrollcommand=sb.set)
         
         
     def create_userSelection(self):
         
         #create text vars
         self.textvars = []
-        for i in range(0, 4):
+        for i in range(0, 20):
             self.textvars.append(tk.StringVar())
-            self.textvars[i].set('nobadwordnobadwordnobadword') 
+            self.textvars[i].set('nobadwordnobadwordnobadwordnobadwordnobad \
+            nobadwordnobadwordnobadwordnobadwordnobadwordnobadwordnobadword \
+            nobadwordnobadwordnobadwordnobadwordnobadwordnobadwordnobadword') 
             
         
-        #holding frame, bottom 1/4 of screen
-        self.panel_two = tk.Frame(self.master, background='black')        
-        self.panel_two.grid(row=1,column=0, sticky='nsew')
         
-        #set interior panel column dimensions        
-        self.panel_two.grid_columnconfigure(1, weight=1)
+        #holding canvas, bottom 1/4 of screen
+        self.panel_two = tk.Canvas(self.master, background='green')        
+        self.panel_two.grid(row=1,column=0, sticky='nsew')                
         
+        #frame inside the canvas to hold our button/label grid
+        self.p2_frame = tk.Frame(self.panel_two, background='black')
+        self.p2_frame.grid(row=0,column=0, sticky='nsew') 
+        #in the frame, want the label to expand, not button
+        self.p2_frame.grid_columnconfigure(1, weight=1)                
+        
+        #variable size widget and command handlers
         cmdHandlers = []
-        self.panel_two.buttons = []        
-        self.panel_two.text_labels = []               
+        self.p2_frame.buttons = []        
+        self.p2_frame.text_labels = []               
         
-        for i in range(0,4):
+        for i in range(0,20):
           
             cmdHandlers.append(ButtonCmd(i, self.textvars[i], self.panel_one.mPrompt))
             
-            self.panel_two.buttons.append(tk.Button(self.panel_two, text=str(i+1), 
+            self.p2_frame.buttons.append(tk.Button(self.p2_frame, text=str(i+1), 
             bg='black', fg="red", command=cmdHandlers[i].button_cmd))
             
-            self.panel_two.text_labels.append(tk.Label(self.panel_two, 
+            self.p2_frame.text_labels.append(WrappingLabel(self.p2_frame, 
             textvariable=self.textvars[i], bg='black', fg='white', anchor='nw'))
+            #self.p2_frame.text_labels[i].bind("<Configure>", self.set_label_wrap)
             
-            self.panel_two.grid_rowconfigure(i, weight=1)
-            self.panel_two.buttons[i].grid(row=i, column=0, sticky='nsew')                        
-            self.panel_two.text_labels[i].grid(row=i, column=1, sticky='nsew')                                    
-            
+            #even row weights in the frame
+            self.p2_frame.grid_rowconfigure(i, weight=1)
+            self.p2_frame.buttons[i].grid(row=i, column=0, sticky='nsew')                        
+            self.p2_frame.text_labels[i].grid(row=i, column=1, sticky='nsew')                                    
+          
+        
+        #add the frame with all of it's children to the canvas
+        self.win = self.panel_two.create_window(0, 0, window=self.p2_frame, anchor='nw')
+        
+        #add a scroll bar so we don't bork our dimensions with different choice numbers
+        sb2 = tk.Scrollbar(self.panel_two, command=self.panel_two.yview)
+        sb2.pack(side=tk.RIGHT, fill=tk.Y)
+        
+        #set max scoll distance to equal the Y-size of the internal frame
+        self.panel_two.configure(yscrollcommand=sb2.set, scrollregion=(0,0,0,0))                
+        
+        self.p2_frame.bind("<Configure>",self.OnFrameConfigure)
+        self.panel_two.bind("<Configure>",self.FrameWidth)
+        
     
     def displayLoop(self):
         self.master.mainloop()
         
+    def FrameWidth(self, event):
+        self.panel_two.itemconfig(self.win, width = event.width)
+        
+    def OnFrameConfigure(self, event):
+        self.panel_two.configure(scrollregion=(0,0,self.p2_frame.winfo_width(),self.p2_frame.winfo_height()))
 
 def runprog():
     root = tk.Tk()
     root.geometry('500x500')
     app = Text_Dungeon(master=root)    
     app.displayLoop()
+    
+class WrappingLabel(tk.Label):
+    def __init__(self, master=None, **kwargs):
+        tk.Label.__init__(self, master, **kwargs)
+        self.bind('<Configure>', lambda e: self.config(wraplength=self.winfo_width()))
     
  
 #wrap a command in order to have it effect a variable source
@@ -103,10 +137,12 @@ class ButtonCmd():
     def button_cmd(self):
         
         innum = self.Main_Prompt.size()
-        self.Main_Prompt.insert(innum + 1, 'nafam' + '\n')   
+        self.Main_Prompt.insert(innum + 1, 'nafam'+ str(innum) + '\n')   
         
         if 'FUCK' in self.Text_Var.get():
-            self.Text_Var.set("nobadword")
+            self.Text_Var.set("nobadwordnobadwordnobadwordnobadwordnobadwordnobadwordnobadword \
+            nobadwordnobadwordnobadwordnobadwordnobadwordnobadwordnobadword \
+            nobadwordnobadwordnobadwordnobadwordnobadwordnobadwordnobadword")
         else:
             self.Text_Var.set("FUCK " + str(self.ID))
             
